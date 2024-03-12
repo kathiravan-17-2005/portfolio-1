4caec29a50a1dcba7ac2afe7dcdaaeb555024fb9
kathiravan-17-2005 <162541043+kathiravan-17-2005@users.noreply.github.com>
2024-03-12 06:40:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================= */
+var typed = new Typed(".typing",{
+    strings:[""," Junior Frontend developer"," Junior web Designer","Graphic DesignerEnthusiast"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
